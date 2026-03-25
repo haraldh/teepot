6896b1e87e0540c2d5123894f8cb7bc5688564e1
Harald Hoyer <harald@hoyer.xyz>
2026-03-25 11:41:21 +0100

    chore: remove teepot-vault-related files
    - Deleted all `teepot-vault` crates, configurations, and related files.
    - Removed associated Docker, Kubernetes, and Nix configurations.
    - This simplifies and removes dependencies on teepot-vault infrastructure.

--- packages/teepotCrate/default.nix
+++ packages/teepotCrate/default.nix
@@ -1,15 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2024 Matter Labs
-{ lib
-, inputs
-, stdenv
-, makeRustPlatform
-, nixsgx ? null
-, pkg-config
-, rust-bin
-, pkgs
-, openssl
-, darwin
+{
+  lib,
+  inputs,
+  stdenv,
+  makeRustPlatform,
+  nixsgx ? null,
+  pkg-config,
+  rust-bin,
+  pkgs,
+  openssl,
+  darwin,
 }:
 let
   rustVersion = rust-bin.fromRustupToolchainFile (inputs.src + "/rust-toolchain.toml");
@@ -32,28 +33,29 @@ let
       nixsgx.sgx-dcap
       nixsgx.sgx-dcap.quote_verify
       nixsgx.sgx-dcap.libtdx_attest
-    ] ++ lib.optionals stdenv.isDarwin [
+    ]
+    ++ lib.optionals stdenv.isDarwin [
       darwin.apple_sdk.frameworks.Security
     ];
 
     strictDeps = true;
 
-    src = with lib.fileset; toSource {
-      root = inputs.src;
-      fileset = unions [
-        # Default files from crane (Rust and cargo files)
-        (craneLib.fileset.commonCargoSources inputs.src)
-        (fileFilter (file: file.hasExt "hcl") (inputs.src + "/crates/teepot-vault/bin"))
-        # deny.toml and friends
-        (fileFilter (file: file.hasExt "toml") inputs.src)
-        # Custom test data files
-        (maybeMissing (inputs.src + "/crates/teepot/tests/data"))
-        (maybeMissing (inputs.src + "/crates/teepot-vault/tests/data"))
-        (maybeMissing (inputs.src + "/crates/intel-dcap-api/tests/test_data"))
-        # special files
-        (maybeMissing (inputs.src + "/crates/teepot-tdx-attest-sys/bindings.h"))
-      ];
-    };
+    src =
+      with lib.fileset;
+      toSource {
+        root = inputs.src;
+        fileset = unions [
+          # Default files from crane (Rust and cargo files)
+          (craneLib.fileset.commonCargoSources inputs.src)
+          # deny.toml and friends
+          (fileFilter (file: file.hasExt "toml") inputs.src)
+          # Custom test data files
+          (maybeMissing (inputs.src + "/crates/teepot/tests/data"))
+          (maybeMissing (inputs.src + "/crates/intel-dcap-api/tests/test_data"))
+          # special files
+          (maybeMissing (inputs.src + "/crates/teepot-tdx-attest-sys/bindings.h"))
+        ];
+      };
 
     checkType = "debug";
     env = {
@@ -62,14 +64,19 @@ let
     };
   };
 
-  cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
-    pname = "teepot-workspace";
-  });
+  cargoArtifacts = craneLib.buildDepsOnly (
+    commonArgs
+    // {
+      pname = "teepot-workspace";
+    }
+  );
 in
 {
-  inherit rustPlatform
+  inherit
+    rustPlatform
     rustVersion
     commonArgs
     craneLib
-    cargoArtifacts;
+    cargoArtifacts
+    ;
 }
