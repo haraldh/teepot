12d6fc2279f1d03e0697a6db97b4cd969c6037eb
Harald Hoyer <harald@hoyer.xyz>
2026-04-07 15:06:00 +0200

    chore: fmt

--- packages/container-tdx-test/default.nix
+++ packages/container-tdx-test/default.nix
@@ -10,28 +10,28 @@
 , nixsgx
 }:
 if (stdenv.hostPlatform.system != "x86_64-linux") then { } else
-  dockerTools.buildLayeredImage {
-    name = "tdx-test";
+dockerTools.buildLayeredImage {
+  name = "tdx-test";
 
-    config.Entrypoint = [ "${teepot.teepot.tdx_test}/bin/tdx-test" ];
-    config.Env = [
-      "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
+  config.Entrypoint = [ "${teepot.teepot.tdx_test}/bin/tdx-test" ];
+  config.Env = [
+    "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
+  ];
+
+  contents = buildEnv {
+    name = "image-root";
+
+    paths = with dockerTools; [
+      teepot.teepot.tdx_test
+      openssl.out
+      curl.out
+      # nixsgx.sgx-dcap.quote_verify
+      # nixsgx.sgx-dcap.default_qpl
+      usrBinEnv
+      binSh
+      caCertificates
+      fakeNss
     ];
-
-    contents = buildEnv {
-      name = "image-root";
-
-      paths = with dockerTools; [
-        teepot.teepot.tdx_test
-        openssl.out
-        curl.out
-        # nixsgx.sgx-dcap.quote_verify
-        # nixsgx.sgx-dcap.default_qpl
-        usrBinEnv
-        binSh
-        caCertificates
-        fakeNss
-      ];
-      pathsToLink = [ "/bin" "/lib" "/etc" "/share" ];
-    };
-  }
+    pathsToLink = [ "/bin" "/lib" "/etc" "/share" ];
+  };
+}
